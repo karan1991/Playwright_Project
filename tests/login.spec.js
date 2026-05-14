03a74429f80a30272aeb54e04d0ccb293b3b19c3
amit.chaurasia <Amit@MAC-YG3QC.local>
2026-05-14 18:02:32 +0530

    adding file

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -14,7 +14,7 @@ test('successful login', async ({ page }) => {
 });
 
 test('understand',async()=>{
-    const browser=await chromium.launch({headless:false});
+    const browser=await chromium.launch({headless:true});
     const context=await browser.newContext();
     const page=await context.newPage();
     await page.goto('https://www.saucedemo.com/');
