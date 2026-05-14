bace58c6b3a16ece8d498578c0512608479faba6
amit.chaurasia <Amit@MAC-YG3QC.local>
2026-05-14 17:36:47 +0530

    adding file

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -49,12 +49,12 @@ export default defineConfig({
       testMatch: /auth.setup.js/,
       use: { ...devices['Desktop Firefox'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'],
-        storageState: 'playwright/.auth/user.json', },
-      dependencies: ['setup'], 
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'],
+    //     storageState: 'playwright/.auth/user.json', },
+    //   dependencies: ['setup'], 
+    // },
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
